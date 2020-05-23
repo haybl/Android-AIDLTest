2262eebd5e40376e2cc679aaba4a05d53850354c
haybl <hayblcurry@qq.com>
2020-05-23 17:14:55 +0800

    init project

---
+++ app/src/main/java/com/haybl/aidl/MainActivity.java
@@ -0,0 +1,17 @@
+package com.haybl.aidl;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * @author haybl
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
